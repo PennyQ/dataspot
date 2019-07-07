4f5191b63b8abeca45af4eea89c372ba0c185652
patrickdehoon <patrick.de.hoon@nl.abnamro.com>
2019-07-07 17:56:30 +0200

    new parsers

--- dataspot/parser/sql/create_as_parser.py
+++ dataspot/parser/sql/create_as_parser.py
@@ -2,97 +2,114 @@
 
 class CreateAsParser:
     def __init__(self, lines):
-        self.end = None
-        self.lines = lines
-        self.count = 0
-        self.query_found = 0
-        self.query = ""
-        self.creates = list()
-        self.start_found = 0
-        self.end_found = 0
+        self._length = None
+        self._lines = lines
+        self._count = 0
+        self._query_found = 0
+        self._query = ""
+        self._creates = list()
+        self._start_found = 0
+        self._end_found = 0
 
-    def set_end(self, parser):
-        self.end = len(parser['create'][0]) - 1
+    def set_length(self, parser):
+        self._length = len(parser['create'][0]) - 1
 
     def find_creates(self, parser):
         # As long as not all items from the first list have been found, we stay in the first part of the if/else
         # statement
-        if self.count <= self.end:
+        if self._count <= self._length:
             # For the 'create table ... as' statement we want to find the statement 'create table' on the first line,
             # and the 'as' statement either on the first line as well, or on the next line. If neither of that is the
             # case, we state that the current statement is not a 'create table ... as' statement. In that case, we
             # reset the configurations
-            if self.count == 0 and self.lines[0].lower().find(parser['create'][0][self.count]) != -1:
-                self.query += self.lines[0].lower()
-                self.count += 1
-                self.lines.pop(0)
-                self.start_found = 1
+            if self._count == 0 and self._lines[0].lower().find(parser['create'][0][self._count]) != -1:
+                self._query += self._lines[0].lower()
+                self._count += 1
+                self._lines.pop(0)
+                self._start_found = 1
                 # If we have not yet found the second item in the current query, we need to check if it is in the
                 # next line
-                if self.count == 1 and self.start_found == 1 and self.query.find(parser['create'][0][self.count]) == -1:
+                if self._count == 1 and self._start_found == 1 and self._query.find(parser['create'][0][self._count]) == -1:
                     # If the second item is in the next line then we know that we have the type of statement we are
                     # after in this parser.
-                    if self.lines[0].lower().find(parser['create'][0][self.count]) != -1:
-                        self.query += self.lines[0].lower()
-                        self.count += 1
-                        self.lines.pop(0)
+                    if self._lines[0].lower().find(parser['create'][0][self._count]) != -1:
+                        self._query += self._lines[0].lower()
+                        self._count += 1
+                        self._lines.pop(0)
                     # If the second item is not in the parser then we don't have a "create table ... as" statement.
                     # In that case, we need to reset our configurations
                     else:
-                        self.query = ""
-                        self.count = 0
-                        self.end_found = 0
-                        self.start_found = 0
-                        self.lines.pop(0)
+                        self._query = ""
+                        self._count = 0
+                        self._end_found = 0
+                        self._start_found = 0
+                        self._lines.pop(0)
                 # In the case, we already have the second item in our query, we can continue parsing the rest. We only
                 # need to up the count in that case
-                elif self.count == 1 and self.start_found == 1 and self.query.find(parser['create'][0][self.count]) != -1:
-                    self.count += 1
-                return self.query
+                elif self._count == 1 and self._start_found == 1 and self._query.find(parser['create'][0][self._count]) != -1:
+                    self._count += 1
+                return self._query
             # The below statements are extra. In case, we want to check for extra arguments, without specific rules,
             # we can use the statements below.
-            elif self.lines[0].lower().find(parser['create'][0][self.count]) == -1 and self.start_found == 1:
-                self.query += self.lines[0].lower()
-                self.lines.pop(0)
-                return self.query
+            elif self._lines[0].lower().find(parser['create'][0][self._count]) == -1 and self._start_found == 1:
+                self._query += self._lines[0].lower()
+                self._lines.pop(0)
+                # In case we find the closing statement without having found all of the items in our first list, we need
+                # to exit this query, since it does not fit our definitions.
+                if self._query.find(parser['create'][1][0]) != -1:
+                    self._query = ""
+                    self._count = 0
+                    self._end_found = 0
+                    self._start_found = 0
+                    self._lines.pop(0)
+                    return self._query
+            # If we have found the argument, we move on to the next argument.
+            elif self._lines[0].lower().find(parser['create'][0][self._count]) != -1 and self._start_found == 1:
+                self._query += self._lines[0].lower()
+                self._lines.pop(0)
+                self._count += 1
+                return self._query
+            # This statement is to make sure we will not end up in an infinite loop.
             else:
-                self.lines.pop(0)
-                return self.query
+                self._lines.pop(0)
+                return self._query
         # When we have found all of the items in the list, we want to find the closing item of the query.
-        elif self.count > self.end:
+        elif self._count > self._length:
             # First we check if we have already found the closing item.
-            if self.query.find(parser['create'][1][0]) != -1:
-                self.end_found = 1
-                return self.query
+            if self._query.find(parser['create'][1][0]) != -1:
+                self._end_found = 1
+                return self._query
             # Then we check if the closing statement is in the current line.
-            elif self.lines[0].lower().find(parser['create'][1][0]) != -1:
-                self.query += self.lines[0].lower()
-                self.lines.pop(0)
-                self.end_found = 1
-                return self.query
+            elif self._lines[0].lower().find(parser['create'][1][0]) != -1:
+                self._query += self._lines[0].lower()
+                self._lines.pop(0)
+                self._end_found = 1
+                return self._query
             # If all is not the case, we continue with the lines until we have found the closing item.
             else:
-                self.query += self.lines[0].lower()
-                self.lines.pop(0)
-                return self.query
+                self._query += self._lines[0].lower()
+                self._lines.pop(0)
+                return self._query
+        # This statement is to make sure we will not end up in an infinite loop.
         else:
-            return self.query
+            self._lines.pop(0)
+            return self._query
 
     def set_creates(self, parser):
         # As longs as we have lines to go through, we continue looping. Since the logic in the above stated function
         # makes sure that all lines will get popped, we don't have to worry about an infinite loop.
-        while len(self.lines) > 0:
+        while len(self._lines) > 0:
             query = self.find_creates(parser=parser)
             # When the closing item of a query has been found, we need to append the query to the list, and reset our
             # configurations so we can find the other statements
-            if self.end_found == 1:
-                self.creates.append(query)
-                self.query = ""
-                self.count = 0
-                self.end_found = 0
-                self.start_found = 0
+            if self._end_found == 1:
+                self._creates.append(query)
+                self._query = ""
+                self._count = 0
+                self._end_found = 0
+                self._start_found = 0
 
     def execute(self, parser):
-        self.set_end(parser=parser)
+        self.set_length(parser=parser)
         self.set_creates(parser=parser)
-        return self.creates
+        return self._creates
--- dataspot/parser/sql/insert_into_parser.py
+++ dataspot/parser/sql/insert_into_parser.py
@@ -3,4 +3,116 @@
 class InsertIntoParser:
 
     def __init__(self, lines):
-        self.lines = lines
+        self._length = None
+        self._lines = lines
+        self._count = 0
+        self._query_found = 0
+        self._query = ""
+        self._inserts = list()
+        self._start_found = 0
+        self._end_found = 0
+
+    def set_length(self, parser):
+        self._length = len(parser['insert'][0]) - 1
+
+    def find_inserts(self, parser):
+        # As long as not all items from the first list have been found, we stay in the first part of the if/else
+        # statement
+        if self._count <= self._length:
+            # For the 'create table ... as' statement we want to find the statement 'create table' on the first line,
+            # and the 'as' statement either on the first line as well, or on the next line. If neither of that is the
+            # case, we state that the current statement is not a 'create table ... as' statement. In that case, we
+            # reset the configurations
+            if self._count == 0 and self._lines[0].lower().find(parser['insert'][0][self._count]) != -1:
+                self._query += self._lines[0].lower()
+                self._count += 1
+                self._lines.pop(0)
+                self._start_found = 1
+                # If we have not yet found the second item in the current query, we need to check if it is in the
+                # next line
+                if self._count == 1 and self._start_found == 1 and self._query.find(
+                        parser['insert'][0][self._count]) == -1:
+                    # If the second item is in the next line then we know that we have the type of statement we are
+                    # after in this parser.
+                    if self._lines[0].lower().find(parser['insert'][0][self._count]) != -1:
+                        self._query += self._lines[0].lower()
+                        self._count += 1
+                        self._lines.pop(0)
+                    # If the second item is not in the parser then we don't have a "create table ... as" statement.
+                    # In that case, we need to reset our configurations
+                    else:
+                        self._query = ""
+                        self._count = 0
+                        self._end_found = 0
+                        self._start_found = 0
+                        self._lines.pop(0)
+                # In the case, we already have the second item in our query, we can continue parsing the rest. We only
+                # need to up the count in that case
+                elif self._count == 1 and self._start_found == 1 and self._query.find(
+                        parser['insert'][0][self._count]) != -1:
+                    self._count += 1
+                return self._query
+            # The below statements are extra. In case, we want to check for extra arguments, without specific rules,
+            # we can use the statements below.
+            elif self._lines[0].lower().find(parser['insert'][0][self._count]) == -1 and self._start_found == 1:
+                self._query += self._lines[0].lower()
+                self._lines.pop(0)
+                # In case we find the closing statement without having found all of the items in our first list, we need
+                # to exit this query, since it does not fit our definitions.
+                if self._query.find(parser['insert'][1][0]) != -1:
+                    self._query = ""
+                    self._count = 0
+                    self._end_found = 0
+                    self._start_found = 0
+                    self._lines.pop(0)
+                    return self._query
+            # If we have found the argument, we move on to the next argument.
+            elif self._lines[0].lower().find(parser['insert'][0][self._count]) != -1 and self._start_found == 1:
+                self._query += self._lines[0].lower()
+                self._lines.pop(0)
+                self._count += 1
+                return self._query
+            # This statement is to make sure we will not end up in an infinite loop.
+            else:
+                self._lines.pop(0)
+                return self._query
+        # When we have found all of the items in the list, we want to find the closing item of the query.
+        elif self._count > self._length:
+            # First we check if we have already found the closing item.
+            if self._query.find(parser['insert'][1][0]) != -1:
+                self._end_found = 1
+                return self._query
+            # Then we check if the closing statement is in the current line.
+            elif self._lines[0].lower().find(parser['insert'][1][0]) != -1:
+                self._query += self._lines[0].lower()
+                self._lines.pop(0)
+                self._end_found = 1
+                return self._query
+            # If all is not the case, we continue with the lines until we have found the closing item.
+            else:
+                self._query += self._lines[0].lower()
+                self._lines.pop(0)
+                return self._query
+        # This statement is to make sure we will not end up in an infinite loop.
+        else:
+            self._lines.pop(0)
+            return self._query
+
+    def set_inserts(self, parser):
+        # As longs as we have lines to go through, we continue looping. Since the logic in the above stated function
+        # makes sure that all lines will get popped, we don't have to worry about an infinite loop.
+        while len(self._lines) > 0:
+            query = self.find_inserts(parser=parser)
+            # When the closing item of a query has been found, we need to append the query to the list, and reset our
+            # configurations so we can find the other statements
+            if self._end_found == 1:
+                self._inserts.append(query)
+                self._query = ""
+                self._count = 0
+                self._end_found = 0
+                self._start_found = 0
+
+    def execute(self, parser):
+        self.set_length(parser=parser)
+        self.set_inserts(parser=parser)
+        return self._inserts
