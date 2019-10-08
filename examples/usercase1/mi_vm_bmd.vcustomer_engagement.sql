6465d2741df69fa654ad996bb5d7480e50d88122
Xuran Qian <C67429@launcher.int.abnamro.com>
2019-10-08 09:50:33 +0200

    'whatever'

--- examples/usercase1/mi_vm_bmd.vcustomer_engagement.sql
+++ examples/usercase1/mi_vm_bmd.vcustomer_engagement.sql
@@ -1,22 +1,2 @@
 #DATASPOT-TERADATA
-replace view Mi_vm_bmd.vcustomer_engagement as (sel * from mi_retail_uia.vcustomer_engagement)
-#DATASPOT-TERADATA
-replace view Mi_vm_bmd.vcustomer_engagement as (sel * from mi_retail_uia.vcustomer_engagement)
-#DATASPOT-TERADATA
-replace view Mi_vm_bmd.vcustomer_engagement as (sel * from mi_retail_uia.vcustomer_engagement)
-#DATASPOT-TERADATA
-replace view Mi_vm_bmd.vcustomer_engagement as (sel * from mi_retail_uia.vcustomer_engagement)
-#DATASPOT-TERADATA
-replace view Mi_vm_bmd.vcustomer_engagement as (sel * from mi_retail_uia.vcustomer_engagement)
-#DATASPOT-TERADATA
-replace view Mi_vm_bmd.vcustomer_engagement as (sel * from mi_retail_uia.vcustomer_engagement)
-#DATASPOT-TERADATA
-replace view Mi_vm_bmd.vcustomer_engagement as (sel * from mi_retail_uia.vcustomer_engagement)
-#DATASPOT-TERADATA
-replace view Mi_vm_bmd.vcustomer_engagement as (sel * from mi_retail_uia.vcustomer_engagement)
-#DATASPOT-TERADATA
-replace view Mi_vm_bmd.vcustomer_engagement as (sel * from mi_retail_uia.vcustomer_engagement)
-#DATASPOT-TERADATA
-replace view Mi_vm_bmd.vcustomer_engagement as (sel * from mi_retail_uia.vcustomer_engagement)
-#DATASPOT-TERADATA
-replace view Mi_vm_bmd.vcustomer_engagement as (sel * from mi_retail_uia.vcustomer_engagement)
+replace view Mi_vm_bmd.vcustomer_engagement as (sel * from mi_retail_uia.vcustomer_engagement);
